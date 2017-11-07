1112317619a1f141de6b1e44e24f05dd4038b671
TomasM <tomasm@tomassystem3.0>
2017-11-06 23:18:55 -0300

    6/11/17

---
+++ js/builder.js
@@ -0,0 +1,4 @@
+// Esqueleto Anuncio
+var anuncio {
+	
+}
--- js/script.js
+++ js/script.js
@@ -9,14 +9,3 @@ var config = {
 };
 
 $(document).ready(firebase.initializeApp(config));
-
-
-// Calendario
-  $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
-    clear: 'Clear',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
-  });
